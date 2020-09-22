fbb0621c290bbd3f62f47556533b55ea467e46ea
unknown <huyuchen012@163.com>
2020-09-22 06:30:45 -0700

    V1.0

---
+++ js/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+	
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	})
+
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer", "Student"],
+		typeSpeed: 150,
+		loop: true,
+		startDelay: 1500, 
+	});
+
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+        	items:4
+        },
+    }
+	});
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    $(window).scroll(function() {
+
+    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 100) {
+	    	
+	    	$('.chart').easyPieChart({
+	        easing: 'easeInOut',
+	        barColor: '#fff',
+	        trackColor: 'black',
+	        scaleColor: false,
+	        lineWidth: 6,
+	        size: 152,
+	        onStep: function(from, to, percent) {
+	        	$(this.el).find('.percent').text(Math.round(percent));
+        		}
+    		});
+    	
+    	}
+    
+    });
+
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+    	filter: '*',
+    	animationOptions: {
+    		duration: 1500,
+    		easing: 'linear',
+    		queue: false
+    	}
+    });
+
+    $("#filters a").click(function() {
+    	$("#filters .current").removeClass("current");
+    	$(this).addClass("current");
+
+    	var selector = $(this).attr("data-filter");
+
+    	$(".items").isotope( {
+    	filter: selector,
+    	animationOptions: {
+    		duration: 1500,
+    		easing: 'linear',
+    		queue: false
+    	}
+    	});
+
+    	return false;
+
+    });
+
+});
